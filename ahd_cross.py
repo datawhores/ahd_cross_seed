047493d09cde65dc98fb41a380dc5f4d4cc42fe9
Johnny smith <lovesickjaguar4@riseup.net>
2020-12-22 00:55:27 +0000

    remove unneeded scan options

--- ahd_cross.py
+++ ahd_cross.py
@@ -1,61 +1,79 @@
 #! /usr/bin/env python3
 """NOTE: READ DOCUMENTATION BEFORE USAGE.
 Usage:
-    ahd_cross.py [--txt=<txtlocation>][--radarrt <movie_root(s)>]... [--sonarrt <tv_root(s)>]... [--normalrt <normal_root(s)>]...
-    [--ignore <sub_folders_to_ignore>]...[--exclude <source_excluded>]...
-    [--config <config>][--delete][--fd <binary_fd> --fdignore <gitignore_style_ignorefile> --lines-skip <num_lines_skipped>]
+    ahd_cross.py [(-h | --help) --txt=<txtlocation> --config <config> --delete --lines-skip <num_lines_skipped]
     [--torrent <torrents_download> --cookie <cookie> --output <output> --api <apikey> --date <int> --size <t_or_f> --misstxt <output>]
-    ahd_cross.py interactive [--txt=<txtlocation>][--radarrt <movie_root(s)>]... [--sonarrt <tv_root(s)>]... [--normalrt <normal_root(s)>]...
-    [--ignore <sub_folders_to_ignore>]...[--exclude <source_excluded>]...
-    [--config <config>][--delete][--fd <binary_fd> --fdignore <gitignore_style_ignorefile> --lines-skip <num_lines_skipped>]
+    [--root <normal_root(s)>...  --ignore <sub_folders_to_ignore>...  --exclude <source_excluded>...]
+    ahd_cross.py interactive [--txt=<txtlocation> --config <config> --delete --lines-skip <num_lines_skipped]
     [--torrent <torrents_download> --cookie <cookie> --output <output> --api <apikey> --date <int> --size <t_or_f> --misstxt <output>]
-    ahd_cross.py (-h | --help)
-    ahd_cross.py scan [--txt=<txtlocation>]
-    [--radarrt <movie_root(s)>]... [--sonarrt <tv_root(s)>]... [--normalrt <normal_root(s)>]... [--ignore <sub_folders_to_ignore>]...
-    [--config <config>][--delete][--fd <binary_fd> --fdignore <gitignore_style_ignorefile> ]
-    ahd_cross.py grab [--txt=<txtlocation> --lines-skip <num_lines_skipped>][--torrent <torrents_download> --cookie <cookie> --output <output> --api <apikey>]
-    [--config <config>][--date <int> --fd <binary_fd> --size <t_or_f>][--exclude <source_excluded>]...
-    ahd_cross.py missing [--txt=<txtlocation> --misstxt <output>  --api <apikey>][--config <config>]
-    ahd_cross.py dedupe --txt=<txtlocation>
-
-
+    [--root <normal_root(s)>...  --ignore <sub_folders_to_ignore>...  --exclude <source_excluded>...]
+    ahd_cross.py scan [--txt=<txtlocation> --config <config> --delete]
+    [--root <normal_root(s)>... --ignore <sub_folders_to_ignore>...]
+    ahd_cross.py grab [--txt=<txtlocation> --lines-skip <num_lines_skipped> --torrent <torrents_download> --cookie <cookie> --output <output> ]
+    [--api <apikey> --config <config> --date <int> --size <t_or_f>]
+    [--exclude <source_excluded>]...
+    ahd_cross.py missing [--txt=<txtlocation> --misstxt <output>  --api <apikey> --config <config>]
+    [--exclude <source_excluded>...]
 
 
 
 Options:
-  -h --help     Show this screen.
- --txt <txtlocation>  txt file with all the file names(required for all commands)
---fd <binary_fd> fd is a program to scan for files, use this if you don't have fd in path,(optional)   [default: fd]
---config ; -x <config> commandline overwrites config
---fdignore <gitignore_style_ignorefile> fd .fdignore file used by fd tto find which folders to ignore, on linux it defaults to the home directory.
-other OS may need to input this manually
+  --h ;--help     Show this screen
+  --config ; -x <config> load arguments from a config file. Can be used alternatively/with ALL required arguments
+  --txt <txtlocation>  txt file with all the file/directory name. Required for scan or grab
 
 
- ahd_cross.py scan scan tv or movie folders root folder(s) create a list of directories. 'txt file creator'. Need at least 1 root.
---sonarrt <tv_root(s)> These are sonnarr type folders with the files with in a "season **" type folders. Will only write directories
---radarrt <movie_root(s)> These are radarr type folders with the files in a file that ends in the year. Will only write directories
---normalrt <normal_root(s)> This Types of folders are not radarr or sonarr folders. Every Item in this folder will be added to the scan list. Max depth of 1
---delete; -d  Will delete the old txt file(optional)
---ignore ; -i <sub_folders_to_ignore>  folder will be ignored for scan (optional)
+========================================================================================================================================================
+[interactive]
+Note:arguments can be blank for this action
+Will display a GUI on the commandline.
+
+<pick 1>
+ahd_cross.py [arguments]
+ahd_cross.py interactive [arguments]
+========================================================================================================================================================
+
+[scan]
+ scan a directory to create a document with all folders and files
+
+ ahd_cross.py scan [arguments]
+
+<required arguments>
+--root <normal_root(s)> root folder to scan
 
 
- ahd_cross.py grab downloads torrents using txt file option to download torrent with --cookie and/or output to file.
-  --lines-skip <num_lines_skipped> Number of lines in txt file to skip during grab  [default: 0]
-  --torrent ; -t <torrents_download>  Here are where the torrent files will download
-  --cookie ; -c <cookie> This is a cookie file for ahd, their are numerous extensions to grab this.
-  --output ; -o <output>  Here are where the torrentlinks will be weritte
-  --date ; -d <int> only download torrents newer then this input should be int, and represents days. By default it is set to around 25 years(optional)  [default: 10000 ]
-  --api ; -a <apikey> This is your ahd passkey
-  --exclude ; -e <source_excluded>  These file type(s) will not be scanned blu,tv,remux,other,web.(optional)
-  --size ; -s <t_or_f> set whether a search should be done by name only or include file size restriction. If true then secondary check will be added to find a match [default: t]
-  "1080p Remux Files,2160 Remux Files or etc files" in a directory match the size of the ahd response(optional)   [default: 1]
 
-  ahd_cross.py missing
-  --misstxt <txt_where_potential_uploads_are written> here we output to a txt file files that don't have any uploads. This means that we can potentially upload these, for rank. Or to increase the amount of cross seeds we have
+<optional arguments>
+--delete; -d  Will delete the old txt file
+--ignore ; -i <sub_folders_to_ignore>  folder will be ignored for scan
+========================================================================================================================================================
+[grab]
+Downloads and/or send to output txt file torrents using input txt file.
+
+ahd_cross.py grab
+<required arguments>
+--cookie  <cookie> This is a cookie file for ahd, their are numerous extensions to grab this.
+--api ; -a <apikey> This is your ahd passkey
 
 
-  ahd_cross.py dedupe
-  Just a basic script to remove duplicate entries from the list. scan will automatically run this after it finishes
+<choose at least 1>
+--output ; -o <output>  Here are where the torrentlinks will be written
+--torrent ; -t <torrents_download>  Here are where the torrent files will downloa
+
+ <optional>
+--date ; -d <int> only download torrents newer then this input should be int, and represents days. By default it is set to around 25 years(optional)  [default: 10000 ]
+--lines-skip <num_lines_skipped> Number of lines in txt file to skip during grab  [default: 0]
+--exclude ; -e <source_excluded>  These file type(s) will not be scanned blu,tv,remux,other,web.(optional)
+--size ; -s <t_or_f> set whether a search should be done by name only or include file size restriction. If true then secondary check will be added to find a match [default: t]
+========================================================================================================================================================
+[missing]
+Checks to see if you have any potential uploads by comparing your files to what has already been uploaded
+
+ahd_cross.py missing
+--misstxt <txt_where_potential_uploads_are written> here we output to a txt file files that don't have any uploads. This means that we can potentially upload these, for rank. Or to increase the amount of cross seeds we have
+
+
+
 
   """
 import requests
@@ -77,12 +95,22 @@ from folders import *
 from classes import *
 from files import *
 from prompt_toolkit.shortcuts import button_dialog
-
+import sys
 
 
 """
 Setup Function
 """
+def setFD():
+    if sys.platform=="linux":
+        try:
+            subprocess.run(['fd'])
+            arguments['--fd']=fd
+        except:
+            path=os.getcwd()
+            fd=f"{path}/Files/fd"
+            print(fd)
+            quit()
 
 def duperemove(txt):
     print("Removing Duplicate lines from ",txt)
@@ -111,8 +139,6 @@ def updateargs(arguments):
         return arguments
     if arguments['--txt']==None:
         arguments['--txt']=config['general']['txt']
-    if arguments['--fd']=="fd":
-        arguments['--fd']=config['general']['fd']
     if arguments['--cookie']==None:
         arguments['--cookie']=config['grab']['cookie']
     if arguments['--api']==None:
@@ -125,12 +151,8 @@ def updateargs(arguments):
         arguments['--misstxt']=config['general']['misstxt']
     if arguments['--exclude']==[] or  arguments['--exclude']==None:
         arguments['--exclude']=config['grab']['exclude']
-    if arguments['--radarrt']==[] or  arguments['--radarrt']==None:
-        arguments['--radarrt']=config['scan']['radarrt']
-    if arguments['--sonarrt']==[] or  arguments['--sonarrt']==None:
-        arguments['--sonarrt']=config['scan']['sonarrt']
-    if arguments['--normalrt']==[] or  arguments['--normalrt']==None:
-        arguments['--normalrt']=config['scan']['normalrt']
+    if arguments['--root']==[] or  arguments['--root']==None:
+        arguments['--root']=config['scan']['root']
     if arguments['--ignore']==[] or arguments['--ignore']==None:
         arguments['--ignore']=config['scan']['ignore']
     if arguments['--size']=="1":
@@ -241,6 +263,8 @@ def setup(arguments,interactive=None):
             return False
 
 def setupscan(arguments):
+    setFD()
+    quit()
     print("Scanning for folders")
     if arguments['--delete']:
         open(file, 'w').close()
@@ -273,43 +297,7 @@ def set_ignored(arguments):
             continue
         ignore.write(element)
         ignore.write('\n')
-def searchtv(arguments,ignorefile):
-  workingdir=os.getcwd()
-  if arguments['--sonarrt']==[] or arguments['--sonarrt']==None or len(arguments['--sonarrt'])==0:
-      return
-  folders=open(arguments['--txt'],"a+")
-  print("Adding TV Folders to",arguments['--txt'])
-  if type(arguments['--sonarrt'])==str:
-      arguments['--sonarrt']=arguments['--sonarrt'].split(",")
-  list=arguments['--sonarrt']
-  for root in list:
-      if root=="":
-          continue
-      if os.path.isdir(root)==False:
-          print(root," is not valid directory")
-          continue
-      t=subprocess.run([arguments['--fd'],'Season\s[0-9][0-9]$','-t','d','.',root,'--max-depth','2','--min-depth','2','--ignore-file',ignorefile],stdout=folders)
-  print("Done")
-def searchmovies(arguments,ignorefile):
-    workingdir=os.getcwd()
-    if arguments['--radarrt']==[] or arguments['--radarrt']==None or len(arguments['--radarrt'])==0:
-        return
-    folders=open(arguments['--txt'],"a+")
-    print("Adding Movies Folders to", arguments['--txt'])
-    if type(arguments['--radarrt'])==str:
-        arguments['--radarrt']=arguments['--radarrt'].split(",")
-    list=arguments['--radarrt']
-    for root in list:
-        if root=="":
-          continue
-        if os.path.isdir(root)==False:
-          print(root," is not valid directory")
-          continue
-    #old scanning method
-    #subprocess.run([arguments['--fd'],'\)$','-t','d','--full-path',root,'--ignore-file',ignorefile],stdout=folders)
-        t=subprocess.run([arguments['--fd'],'-e','.mkv','-t','f','.',root,'--max-depth','2','--ignore-file',ignorefile],stdout=folders)
-    print("Done")
-def searchnormal(arguments,ignorefile):
+def searchdir(arguments,ignorefile):
     if arguments['--normalrt']==[] or arguments['--normalrt']==None:
         return
     folders=open(arguments['--txt'],"a+")
@@ -331,10 +319,10 @@ def searchnormal(arguments,ignorefile):
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='ahd_cross_seed_scan 1.2')
 #interactive Mode
-
+    print(arguments)
     if arguments.get("--config")==None:
         arguments['--config']=os.path.dirname(os.path.abspath(__file__))+"/ahd_cross.txt"
-    if (arguments['scan']!=True and arguments['dedupe']!=True and arguments['grab']!=True and arguments['missing']!=True) or arguments['interactive']:
+    if (arguments['scan']!=True  and arguments['grab']!=True and arguments['missing']!=True) or arguments['interactive']:
             message_dialog(
                 title="Interactive Mode",
                 text="Welcome to AHD Cross you are starting the programs in interactive Mode\nBefore Deciding on the next question note a config File is required in this mode",
@@ -369,20 +357,20 @@ if __name__ == '__main__':
                     setupscan(arguments)
                     set_ignored(arguments)
                     duperemove(arguments['--fdignore'])
-                    searchtv(arguments,arguments['--fdignore'])
-                    searchmovies(arguments,arguments['--fdignore'])
-                    searchnormal(arguments,arguments['--fdignore'])
+                    searchdir(arguments,arguments['--fdignore'])
                     duperemove(arguments['--txt'])
                 elif continueloop=="missing":
                     t=setup(arguments,True)
                     if t==False:
                         continue
+                    setupscan(arguments)
                     missing(arguments)
                     duperemove(arguments['--misstxt'])
                 elif continueloop=="download":
                     t=setup(arguments,True)
                     if t==False:
                         continue
+                    setupscan(arguments)
                     download(arguments,arguments['--txt'])
                 elif continueloop=="config":
                     arguments['--config']=input_dialog(title='Config Path',text='Please Enter the Path to your Config File:').run()
@@ -406,9 +394,7 @@ if __name__ == '__main__':
         setupscan(arguments)
         set_ignored(arguments)
         duperemove(arguments['--fdignore'])
-        searchtv(arguments,arguments['--fdignore'])
-        searchmovies(arguments,arguments['--fdignore'])
-        searchnormal(arguments,arguments['--fdignore'])
+        searchdir(arguments,arguments['--fdignore'])
         duperemove(arguments['--txt'])
     elif arguments['grab']:
         setup(arguments)
@@ -417,5 +403,3 @@ if __name__ == '__main__':
         setup(arguments)
         missing(arguments)
         duperemove(arguments['--misstxt'])
-    elif arguments['dedupe']:
-        duperemove(arguments['--txt'])
